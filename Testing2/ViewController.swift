a470eb7e5386d409192233e15f922d66629b16e9
Pierce Findlay <piercefindlay@Pierces-MacBook-Pro.local>
2017-04-18 15:50:22 -0700

    Initial Commit

---
+++ Testing2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing2
+//
+//  Created by Pierce Findlay on 4/18/17.
+//  Copyright © 2017 Pierce Findlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
